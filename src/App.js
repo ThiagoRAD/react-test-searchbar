457d38ea097b0b12e024dfa12eac6f64fae45d64
ThiagoRAD <thauroo@gmail.com>
2019-05-13 13:04:05 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const getUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <ul>
+        {users
+          .filter((user) => user.name.toLowerCase().includes(search.toLowerCase()))
+          .map((user) => (
+            <li key={`user-${user.id}`}>{user.name}</li>
+          ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
